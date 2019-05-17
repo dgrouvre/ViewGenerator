356533b4477dcb8a0dd07d4abc465b9f193c9626
dgrouvre <seann.vancleve@gmail.com>
2019-05-17 13:47:02 -0500

    Add files via upload

---
+++ ITemplate.cs
@@ -0,0 +1,13 @@
+﻿using Frs.Mpls.Public.Foundation.Orm.Models;
+using Glass.Mapper.Sc.Configuration.Attributes;
+
+namespace Frb.Mpls.Public.Feature.{Template}.Models
+{
+    [SitecoreType]
+    public interface I{Template} : IGlassBase
+    {
+        [SitecoreField]
+        string Title { get; set; }
+
+    }
+}
